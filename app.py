7cfd854e0d790c05454b88f61a759e5930448ad4
DONCHAN7004728 <arpan70047283086@gmail.com>
2025-04-25 21:12:19 +0530

    Update app3

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 st.set_page_config(page_title="Spam Detector", page_icon="🚫", layout="centered")
 
-
 model = load_model('SpamDetectModel.h5', compile=False)
 
 
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
-
+=
 
 st.title("🚫 Spam Message Detector")
 st.markdown("Enter a message below to check if it's spam or not.")
-
 
 user_input = st.text_area("📩 Enter your message here", height=150)
 
@@ -25,12 +23,17 @@ if st.button("Detect Spam"):
         st.warning("Please enter a message first.")
     else:
         sequences = tokenizer.texts_to_sequences([user_input])
-        padded = pad_sequences(sequences, maxlen=100)
-
-
-        prediction = model.predict(padded)[0][0]
-
-        if prediction > 0.5:
-            st.error("🚨 This message is likely **Spam**.")
+  
+        if len(sequences[0]) == 0:
+            st.warning("Input message is too short or contains no recognizable words.")
         else:
-            st.success("✅ This message is **Not Spam**.")
+            padded = pad_sequences(sequences, maxlen=100)
+
+
+            prediction = model.predict(padded)[0][0]
+
+    
+            if prediction > 0.5:
+                st.error("🚨 This message is likely **Spam**.")
+            else:
+                st.success("✅ This message is **Not Spam**.")
