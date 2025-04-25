4ac64f37811abba57a5ddec51d6e718cadbc2a17
DONCHAN7004728 <arpan70047283086@gmail.com>
2025-04-25 21:08:21 +0530

    Update15

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import numpy as np
-from keras.models import load_model
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 st.set_page_config(page_title="Spam Detector", page_icon="🚫", layout="centered")
 
 
 model = load_model('SpamDetectModel.h5', compile=False)
+
 
 tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
 
@@ -23,16 +24,13 @@ if st.button("Detect Spam"):
     if user_input.strip() == "":
         st.warning("Please enter a message first.")
     else:
-
         sequences = tokenizer.texts_to_sequences([user_input])
         padded = pad_sequences(sequences, maxlen=100)
 
 
         prediction = model.predict(padded)[0][0]
 
-    
         if prediction > 0.5:
             st.error("🚨 This message is likely **Spam**.")
         else:
             st.success("✅ This message is **Not Spam**.")
-
